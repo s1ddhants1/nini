d8dbdcb8ef036e5f098f2dc7a731c932069a1b9c
s1ddhants1 <86736295+s1ddhants1@users.noreply.github.com>
2025-02-06 19:57:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ document.addEventListener("DOMContentLoaded", function () {
   let lines = document.querySelectorAll('.typing-animation p');
   let delay = 0;
 
-  lines.forEach((line, index) => {
+  lines.forEach((line) => {
     setTimeout(() => {
       line.style.transition = 'width 2s ease, opacity 1s ease';
       line.style.width = line.scrollWidth + 'px';
       line.style.opacity = 1;
     }, delay);
-    delay += 2500; // Adjust time between each line's appearance
+    delay += 2500;
+  });
+
+  const music = document.getElementById('background-music');
+  const musicToggle = document.getElementById('music-toggle');
+
+  musicToggle.addEventListener('click', function () {
+    if (music.paused) {
+      music.play();
+      musicToggle.textContent = 'Pause Music';
+    } else {
+      music.pause();
+      musicToggle.textContent = 'Play Music';
+    }
   });
 });
