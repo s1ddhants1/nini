8390d4c26d8c701e53bcf80731ba05ca9759540c
s1ddhants1 <86736295+s1ddhants1@users.noreply.github.com>
2025-02-12 00:02:19 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Homepage Elements //
-  // Elements for the hamburger menu
-  const menuIcon = document.querySelector('.menu-icon');
-  const navLinks = document.querySelector('.nav-links');
-  
-  // Handle hamburger menu toggle
-  const toggleMenu = () => {
-    navLinks.classList.toggle('nav-active');
-  };
-  // Toggle menu on click
-  menuIcon.addEventListener('click', () => {
-    navLinks.classList.toggle('nav-active');
-  });
-  
   // Valentine Page Elements //
  document.addEventListener("DOMContentLoaded", function () {
   // Typing Animation
