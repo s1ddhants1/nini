6b96be41df9450418f768529f67797f9ee19d282
s1ddhants1 <86736295+s1ddhants1@users.noreply.github.com>
2025-02-14 03:54:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // ----- HAMBURGER MENU FUNCTIONALITY -----
   const menuIcon = document.querySelector(".menu-icon");
   const navLinks = document.querySelector(".nav-links");
 
-  // Toggle menu on click
   const toggleMenu = () => {
     navLinks.classList.toggle("nav-active");
   };
